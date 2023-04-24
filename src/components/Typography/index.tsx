d9b47aaf276f1c4982b5c1f4670c20c7be3eea52
Jia Yi <ooijiayi93@gmail.com>
2023-04-24 12:07:51 +0800

    update typography

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,7 +1,45 @@
-import tw, { styled } from "twin.macro";
+import tw, { styled, css } from "twin.macro";
 
-export const HeadingL = styled.h1(() => [
-  // The common button styles added with the tw import
-  tw`text-4xl font-bold text-08`,
-  // Some additional component specific styles
+export const HeadingL = styled.h3(() => [
+  tw`text-2xl font-bold text-08 dark:text-white`,
+  css`
+    line-height: 33px;
+    letter-spacing: -1.125px;
+  `,
+]);
+
+export const HeadingM = styled.h4(() => [
+  tw`text-xl font-bold text-08 dark:text-white`,
+  css`
+    line-height: 22px;
+    letter-spacing: -0.75px;
+  `,
+]);
+
+export const HeadingS = styled.h5(({ variant }: { variant?: boolean }) => [
+  tw`text-lg font-bold text-08 dark:text-white`,
+  css`
+    letter-spacing: -0.25px;
+  `,
+  variant
+    ? css`
+        line-height: 15px;
+      `
+    : css`
+        line-height: 24px;
+      `,
+]);
+
+export const Body = styled.p(({ variant }: { variant?: boolean }) => [
+  tw`text-base font-normal text-08 dark:text-white`,
+
+  variant
+    ? css`
+        line-height: 15px;
+        letter-spacing: -0.25px;
+      `
+    : css`
+        line-height: 18px;
+        letter-spacing: -0.1px;
+      `,
 ]);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
+import { Body } from "~/components/Typography";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -49,6 +50,9 @@ const Home: NextPage = () => {
             </p>
             <AuthShowcase />
           </div>
+          <>
+            <Body variant>Testing Body medium</Body>
+          </>
         </div>
       </main>
     </>
