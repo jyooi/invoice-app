c8b1256a27dfc855cf1ba2368a95ee94ab1220a5
OOI JIA YI <ooijiayi93@gmail.com>
2023-08-02 17:45:47 +0800

    Popover component

---
+++ src/components/Popover/index.tsx
@@ -0,0 +1,50 @@
+import { Popover } from "@headlessui/react";
+import ChevronDownSvg from "../../image/Icons/purple_chevron_down_icon.svg";
+import ChevronUpSvg from "../../image/Icons/purple_chevron_up_icon.svg";
+import Image from "next/image";
+import tw, { styled, css } from "twin.macro";
+
+const PopOverButton = styled(Popover.Button)(() => [
+  tw`flex items-center gap-[14px]`,
+
+  css`
+    font-size: 15px;
+    font-weight: 700;
+    line-height: 15px;
+    letter-spacing: -0.25px;
+
+    outline: none;
+  `,
+]);
+
+const PopOverPanel = styled(Popover.Panel)(() => [
+  [
+    tw`bg-white rounded-lg dark:bg-04 inline-block mt-[22px] mr-[30px]`,
+    css`
+      box-shadow: 0px 10px 20px 0px rgba(72, 84, 159, 0.25);
+      position: absolute;
+    `,
+  ],
+]);
+
+type PropType = { label: string; children: React.ReactNode };
+
+export function PopOver({ label, children }: PropType) {
+  return (
+    <Popover>
+      {({ open }) => (
+        /* Use the `open` state to conditionally change the direction of the chevron icon. */
+        <>
+          <PopOverButton>
+            {label}
+            <Image
+              src={open ? (ChevronUpSvg as string) : (ChevronDownSvg as string)}
+              alt="chevronDown"
+            />
+          </PopOverButton>
+          <PopOverPanel>{children}</PopOverPanel>
+        </>
+      )}
+    </Popover>
+  );
+}
--- src/pages/invoice/index.tsx
+++ src/pages/invoice/index.tsx
@@ -2,7 +2,8 @@
 import { Checkbox } from "~/components/Checkbox";
 import { HeadingL, Body } from "~/components/Typography";
 import { useState } from "react";
-
+import { PopOver } from "~/components/Popover";
+import { Button } from "~/components/Button";
 export default function Invoice() {
   // const users = api.user.getAll.useQuery();
 
@@ -14,10 +15,30 @@ export default function Invoice() {
 
   return (
     <div tw="h-screen">
-      <HeadingL>Invoice</HeadingL>
-      <Body>There are 7 total invoice</Body>
-      <Checkbox setIsChecked={setIsCheck} isChecked={isCheck} />
+      <div tw="flex justify-between h-[55px]">
+        <div tw="flex flex-col">
+          <HeadingL>Invoice</HeadingL>
+          <Body>There are 7 total invoice</Body>
+        </div>
+
+        <div tw="flex items-center gap-[40px]">
+          <PopOver label="Filter by status">
+            <div tw="w-48 h-36">
+              <Checkbox setIsChecked={setIsCheck} isChecked={isCheck} />
+              <Checkbox setIsChecked={setIsCheck} isChecked={isCheck} />
+              <Checkbox setIsChecked={setIsCheck} isChecked={isCheck} />
+            </div>
+          </PopOver>
+          <Button
+            addIcon
+            variant="primary"
+            label={"New Invoice"}
+            onClick={function (): void {
+              throw new Error("Function not implemented.");
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
-
