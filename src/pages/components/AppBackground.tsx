c31f39e80fe1cc868b369fe625add4e21a35668e
OOI JIA YI <ooijiayi93@gmail.com>
2023-08-03 17:42:49 +0800

    fix app backgroud default export

--- src/pages/components/AppBackground.tsx
+++ src/pages/components/AppBackground.tsx
@@ -4,6 +4,8 @@ import tw, { styled } from "twin.macro";
 
 const Container = styled.div(() => [tw`bg-11 dark:bg-[#141625]`]);
 
-export const AppBackground = ({ children }: { children: React.ReactNode }) => (
+const AppBackground = ({ children }: { children: React.ReactNode }) => (
   <Container>{children}</Container>
 );
+
+export default AppBackground;
