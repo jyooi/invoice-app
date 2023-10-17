e00ffb8ca2f2be18154fe9d19c6e14d527fdb76f
OOI JIA YI <ooijiayi93@gmail.com>
2023-10-17 16:06:45 +0800

    fix Textfield error type

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -3,7 +3,7 @@ import tw, { styled, css } from "twin.macro";
 import { Label } from "../Label";
 import { type HTMLInputTypeAttribute } from "react";
 
-const TextInput = styled.input(({ error }: { error: boolean }) => [
+const TextInput = styled.input(({ error }: { error?: boolean }) => [
   tw`border-05 border rounded h-12 w-full`,
   tw`focus-visible:outline-02 focus-visible:border-0`,
   tw`dark:bg-03 dark:border-04 focus-visible:dark:outline-0`, // Dark config
@@ -25,6 +25,7 @@ type PropType = {
   label?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   type?: HTMLInputTypeAttribute;
+  error?: boolean;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ref?: any;
 };
@@ -33,7 +34,7 @@ export const TextField = (props: PropType) => {
   return (
     <TextFieldGroup>
       {props.label && <Label>{props.label}</Label>}
-      <TextInput onChange={props.onChange} {...props} />
+      <TextInput error={props.error} onChange={props.onChange} {...props} />
     </TextFieldGroup>
   );
 };
