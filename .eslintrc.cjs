b6d84f4bed74ecb5b1a74cd02701cb07c3daecfe
OOI JIA YI <ooijiayi93@gmail.com>
2023-10-21 12:28:08 +0800

    update linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,6 +30,11 @@ const config = {
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-misused-promises": [2, {
+        "checksVoidReturn": {
+          "attributes": false
+        }
+      }]
   },
 };
 
